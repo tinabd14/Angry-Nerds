97c6af6406945920b178623619191dc13cb0b3eb
Ali-112233 <63885534+Ali-112233@users.noreply.github.com>
2020-04-18 02:53:22 +0300

    Update Main 4.java
    Adding new feature

--- src/Main 4.java
+++ src/Main 4.java
@@ -2,11 +2,17 @@ public class AddTwoIntegers {
 
     public static void main(String[] args) {
         
-        int first = 10;
+        int first = 5;
         int second = 20;
-
+        
+        //Sum of the 2 intgeers is given as 
         int sum = first + second;
+        System.out.println("The sum of "+first " & "+ second+ " is: " + sum);
+        
+        // Difference of 2 integers is given as
+        int diff = first - second;
+        System.out.println("The diff of "+first " & "+ second+ " is: " + diff);
 
-        System.out.println("The sum is: " + sum);
+        
     }
 }
