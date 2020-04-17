e890110969ce587f439a589feee465eb8799c648
khushbakhtali <62602538+khushbakhtali@users.noreply.github.com>
2020-04-18 02:17:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Welcome to the Corona Information Center");
+        System.out.println("Press 1 for World Wide Information. Press 2 for Turkey Death Rate, etc");
+        System.out.println("Stay Home.Stay Safe")
+            
+            
     }
 }
