37bd9f42a67f4d9c84bc22120d2352ca3739249b
Tunar Mahmudov <tunardev14@gmail.com>
2020-05-11 18:23:22 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Welcome to our presentation!");
+        System.out.println("Long Live Github!");
     }
 }
